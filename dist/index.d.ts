f3e566db21fb01490c6f032493bb21014c2173a5
cotttpan <cotttpan@gmail.com>
2017-04-06 20:59:02 +0900

    chore(*): update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,12 +1,22 @@
-declare function iterate<T, R1, R2>(iterable: Iterable<T>, pipe: Pipe<T, R1>, tick: Tick<R1, R2>): R2 | undefined;
-declare function iterate<T, R1>(iterable: Iterable<T>, pipe: Pipe<T, R1>, tick?: Tick<R1, R1>): R1 | undefined;
+declare function iterate<T, R1, R2, R3>(iterable: Iterable<T>, pipe: Pipe<T, R1>, tick: Tick<R1, R2>, done: Done<R2, R3>): R3 | undefined;
+declare function iterate<T, R1, R2>(iterable: Iterable<T>, pipe: Pipe<T, R1>, tick: Tick<R1, R2>, done?: Done<R2, R2>): R2 | undefined;
+declare function iterate<T, R1>(iterable: Iterable<T>, pipe: Pipe<T, R1>, tick?: Tick<R1, R1>, done?: Done<R1, R1>): R1 | undefined;
 declare namespace iterate {
-    function map<T, R1, R2>(iterable: Iterable<T>, fn: Iteratee<T, R1>, tick: Tick<R1[], R2>): R2 | undefined;
-    function map<T, R>(iterable: Iterable<T>, fn?: Iteratee<T, R>, tick?: Tick<R[], R[]>): R[] | undefined;
-    function filter<T, R>(iterable: Iterable<T>, fn: Iteratee<T, boolean>, tick: Tick<T[], R>): R | undefined;
-    function filter<T>(iterable: Iterable<T>, fn?: Iteratee<T, boolean>, tick?: Tick<T[], T[]>): T[] | undefined;
-    function reduce<T, R1, R2>(iterable: Iterable<T>, fn: Reducer<R1, T>, init: R1, tick: Tick<R1, R2>): R2 | undefined;
-    function reduce<T, R>(iterable: Iterable<T>, fn: Reducer<R, T>, init: R, tick?: Tick<R, R>): R | undefined;
+    function map<T, R1, R2, R3>(iterable: Iterable<T>, fn: Iteratee<T, R1>, tick: Tick<R1[], R2>, done: Done<R2, R3>): R3 | undefined;
+    function map<T, R1, R2>(iterable: Iterable<T>, fn: Iteratee<T, R1>, tick: Tick<R1[], R2>, done?: Done<R2, R2>): R2 | undefined;
+    function map<T, R>(iterable: Iterable<T>, fn?: Iteratee<T, R>, tick?: Tick<R[], R[]>, done?: Done<R[], R[]>): R[] | undefined;
+    function filter<T, R, R2>(iterable: Iterable<T>, fn: Iteratee<T, boolean>, tick: Tick<T[], R>, done: Done<R, R2>): R2 | undefined;
+    function filter<T, R>(iterable: Iterable<T>, fn: Iteratee<T, boolean>, tick: Tick<T[], R>, done?: Done<R, R>): R | undefined;
+    function filter<T>(iterable: Iterable<T>, fn?: Iteratee<T, boolean>, tick?: Tick<T[], T[]>, done?: Done<T[], T[]>): T[] | undefined;
+    function reduce<T, R1, R2, R3>(iterable: Iterable<T>, fn: Reducer<R1, T>, init: R1, tick: Tick<R1, R2>, done: Done<R2, R3>): R3 | undefined;
+    function reduce<T, R1, R2>(iterable: Iterable<T>, fn: Reducer<R1, T>, init: R1, tick: Tick<R1, R2>, done?: Done<R2, R2>): R2 | undefined;
+    function reduce<T, R>(iterable: Iterable<T>, fn: Reducer<R, T>, init: R, tick?: Tick<R, R>, done?: Done<R, R>): R | undefined;
+}
+export declare namespace _ {
+    function identity<T>(value: T): T;
+    function existy(v: any): boolean;
+    function constant<T>(value: T): () => T;
+    function tick<T>(next: Function, callbackResult: T | Promise<T>): any;
 }
 export interface Reducer<T, U> {
     (acc: T, value: U, index: number, src: Iterable<U>): T;
@@ -19,6 +29,9 @@ export interface Pipe<A, R> {
 }
 export interface Tick<A, R> {
     (next: <R>(arg: R) => R, arg: A): R | void;
+}
+export interface Done<T, U> {
+    (value: T): U;
 }
 export default iterate;
 export { iterate };
--- dist/index.js
+++ dist/index.js
@@ -1,43 +1,43 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function iterate(iterable, pipe, tick = _.tick) {
+function iterate(iterable, pipe, tick = _.tick, done = _.identity) {
     const iterator = iterable[Symbol.iterator]();
     let index = -1;
     const src = iterable;
     const next = (value) => {
         const ir = iterator.next();
-        return ir.done ? value : tick(next, pipe(ir.value, ++index, src));
+        return ir.done ? done(value) : tick(next, pipe(ir.value, ++index, src));
     };
     return next();
 }
 exports.iterate = iterate;
 (function (iterate) {
-    function map(iterable, fn = _.identity, tick = _.tick) {
+    function map(iterable, fn = _.identity, tick = _.tick, done = _.identity) {
         const acc = [];
         const pipe = function () {
             acc.push(fn.apply(null, arguments));
             return acc;
         };
-        return iterate(iterable, pipe, tick);
+        return iterate(iterable, pipe, tick, done);
     }
     iterate.map = map;
-    function filter(iterable, fn = _.constant(true), tick = _.tick) {
+    function filter(iterable, fn = _.constant(true), tick = _.tick, done = _.identity) {
         const acc = [];
         const pipe = function () {
             if (fn.apply(null, arguments))
                 acc.push(arguments[0]);
             return acc;
         };
-        return iterate(iterable, pipe, tick);
+        return iterate(iterable, pipe, tick, done);
     }
     iterate.filter = filter;
-    function reduce(iterable, fn, init, tick = _.tick) {
+    function reduce(iterable, fn, init, tick = _.tick, done = _.identity) {
         let acc = init;
         const pipe = function () {
             acc = fn.apply(null, [acc, ...arguments]);
             return acc;
         };
-        return iterate(iterable, pipe, tick);
+        return iterate(iterable, pipe, tick, done);
     }
     iterate.reduce = reduce;
 })(iterate || (iterate = {}));
@@ -63,6 +63,6 @@ var _;
         return next(callbackResult);
     }
     _.tick = tick;
-})(_ || (_ = {}));
+})(_ = exports._ || (exports._ = {}));
 exports.default = iterate;
 //# sourceMappingURL=index.js.map
